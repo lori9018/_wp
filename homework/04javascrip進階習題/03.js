c1898dd589af5ef9d692694f7ebe13b5021b42b6
lori9018 <arther20059018@gmail.com>
2025-03-21 12:01:58 +0800

    0321

---
+++ homework/04javascrip進階習題/03.js
@@ -0,0 +1,26 @@
+/*
+3. 計算陣列數字總和
+實作 sumArray(arr)，計算數字陣列的總和
+console.log(sumArray([1, 2, 3, 4])); 
+// 10
+*/
+
+function sumArray(arr){
+    let sum = 0;
+    for(let num of arr){
+        sum += num;
+    }
+    return sum;
+}
+
+console.log(sumArray([1, 2, 3, 4]));
+
+/*
+chatgpt的作法
+function sumArray(arr) {
+    return arr.reduce((sum, num) => sum + num, 0);
+}
+    
+console.log(sumArray([1, 2, 3, 4])); 
+// 10
+*/
